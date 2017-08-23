3c219686d4b5a2fad2a17e9f5a1f19301ea0317b
Gerard KESSE <tiakagerard@hotmail.com>
2017-08-23 16:23:43 +0200

    Initial Commit

--- Foncteur/src/manager/GFunctor.h
+++ Foncteur/src/manager/GFunctor.h
@@ -2,10 +2,6 @@
 #ifndef _GFunctor_
 #define _GFunctor_
 //===============================================
-#include <string>
-//===============================================
-using namespace std;
-//===============================================
 class GFunctor1 {   
 public:
     int operator()(const int& a, const int& b) {
