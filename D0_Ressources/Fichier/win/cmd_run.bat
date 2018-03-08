eefa0aff900687587c8c2b9715f35c84628f0761
Gerard KESSE <tiakagerard@hotmail.com>
2018-03-09 00:15:32 +0100

    Initial Commit

---
+++ D0_Ressources/Fichier/win/cmd_run.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set PATH=C:\MinGW\bin
+set PATH=C:\Users\gerar\Downloads\SDL\SDL2-devel-2.0.8-mingw\SDL2-2.0.8\i686-w64-mingw32\bin;%PATH%
+set PATH=C:\Users\gerar\Downloads\OpenGL\glew-2.1.0-win32\glew-2.1.0\bin\Release\Win32;%PATH%
+
+echo.
+bin\GProject
+echo.
+pause
