88065241ea25c1c3635e1490f730cf77b13fbe44
Gerard KESSE <tiakagerard@hotmail.com>
2020-04-09 15:14:41 +0100

    Initial Commit

--- code/GProject/src/manager/GWindowMath.cpp
+++ code/GProject/src/manager/GWindowMath.cpp
@@ -41,109 +41,101 @@ GWindowMath::~GWindowMath() {
 void GWindowMath::slotRun() {
 	GDebug::Instance()->process("log", __CLASSNAME__, "::", __FUNCTION__, "()", 0);
 	QString lExpression = m_expression->getText();
-	QScriptEngine lScript;
 	if(lExpression == "") return;
 
-	QString expression= lExpression.remove(QRegExp("\\s"));
-	QString exp1,exp2;
+	QString lExpressionIn;
+	QString lExp1, lExp2;
 
-	expression.replace("pi","Math.PI");
-	expression.replace("abs(","Math.abs{");
-	expression.replace("sin(","Math.sin{");
-	expression.replace("cos(","Math.cos{");
-	expression.replace("tan(","Math.tan{");
+	lExpressionIn = lExpression.remove(QRegExp("\\s"));
+
+	lExpressionIn.replace("pi","Math.PI");
+	lExpressionIn.replace("abs(","Math.abs{");
+	lExpressionIn.replace("sin(","Math.sin{");
+	lExpressionIn.replace("cos(","Math.cos{");
+	lExpressionIn.replace("tan(","Math.tan{");
 	//.... and so on for trigonometrical functions)
+	lExpressionIn.replace("sqrt(","Math.sqrt{");
 
-	expression.replace("sqrt(","Math.sqrt{");
-	expression.replace("(","[");
-	expression.replace(")","]");
-	expression.replace("^","POW");
-	expression.replace("+","PLUS");
-	expression.replace("-","MINUS");
-	expression.replace("*","MULT");
-	expression.replace("/","DIV");
+	lExpressionIn.replace("(","[");
+	lExpressionIn.replace(")","]");
+	lExpressionIn.replace("^","POW");
+	lExpressionIn.replace("+","PLUS");
+	lExpressionIn.replace("-","MINUS");
+	lExpressionIn.replace("*","MULT");
+	lExpressionIn.replace("/","DIV");
 
-	while (expression.contains("[")) //START SOLVING EXPRESSIONS BETWEEN BRACKETS
-	{
+	while (lExpressionIn.contains("[")) {
+		QRegularExpression lRegExp("\\[([^\\]]+)\\]");
+		QRegularExpressionMatch lMatchExp = lRegExp.match(lExpressionIn);
+		QString lOrgExp = lMatchExp.captured(0);
+		QString lFindExp = lMatchExp.captured(1);
 
-		QRegularExpression rep("\\[([^\\]]+)\\]");
-		QRegularExpressionMatch matchp = rep.match(expression);
-		QString expp_w_brackets = matchp.captured(0); //original expression with brakets
-		QString expp = matchp.captured(1);//expression without brakets
+		while (lFindExp.contains("POW")) {
+			QRegularExpression lRegExp1("(\\d+.\\d+POW)|(\\d+POW)");
+			QRegularExpressionMatch lMatchExp1 = lRegExp1.match(lFindExp);
 
-		while (expp.contains("POW")) //SOLVE POE EXPRESSION
-		{
-			QRegularExpression re1("(\\d+.\\d+POW)|(\\d+POW)");
-			QRegularExpressionMatch match1 = re1.match(expp);
+			QRegularExpression lRegExp2("(POW\\d+.\\d+)|(POW\\d+)");
+			QRegularExpressionMatch lMathcExp2 = lRegExp2.match(lFindExp);
 
-			QRegularExpression re2("(POW\\d+.\\d+)|(POW\\d+)");
-			QRegularExpressionMatch match2 = re2.match(expp);
-
-			if (match1.hasMatch())
-			{
-				exp1 = match1.captured(0);
-				exp1.replace("POW","");
+			if(lMatchExp1.hasMatch()) {
+				lExp1 = lMatchExp1.captured(0);
+				lExp1.replace("POW","");
 			}
 
-			if (match2.hasMatch())
-			{
-				exp2 = match2.captured(0);
-				exp2.replace("POW","");
+			if(lMathcExp2.hasMatch()) {
+				lExp2 = lMathcExp2.captured(0);
+				lExp2.replace("POW","");
 			}
 
-			expp.replace (""+exp1+"" "POW"""+exp2+"" , "Math.pow(" ""+exp1+"" ","  ""+exp2+""  ")");
+			lFindExp.replace (""+lExp1+"" "POW"""+lExp2+"" , "Math.pow(" ""+lExp1+"" ","  ""+lExp2+""  ")");
 		}
 
 
-		expp.replace ("PLUS","+");
-		expp.replace ("MINUS","-");
-		expp.replace("MULT","*");
-		expp.replace("DIV","/");
+		lFindExp.replace ("PLUS","+");
+		lFindExp.replace ("MINUS","-");
+		lFindExp.replace("MULT","*");
+		lFindExp.replace("DIV","/");
 
-		QScriptEngine parsexpressionp;
-		double resultp=parsexpressionp.evaluate(expp).toNumber();
+		QScriptEngine parslExpressionInp;
+		double resultp=parslExpressionInp.evaluate(lFindExp).toNumber();
 		QString BraketResult=(QString::number(resultp));
-		expression.replace(""+expp_w_brackets+"" ,""+BraketResult+"");
+		lExpressionIn.replace(""+lOrgExp+"" ,""+BraketResult+"");
 	}
 
-	while (expression.contains("POW")) //SOLVE POE EXPRESSION
-	{
-		QRegularExpression re1("(\\d+.\\d+POW)|(\\d+POW)");
-		QRegularExpressionMatch match1 = re1.match(expression);
+	while(lExpressionIn.contains("POW")) {
+		QRegularExpression lRegExp1("(\\d+.\\d+POW)|(\\d+POW)");
+		QRegularExpressionMatch lMatchExp1 = lRegExp1.match(lExpressionIn);
 
-		QRegularExpression re2("(POW\\d+.\\d+)|(POW\\d+)");
-		QRegularExpressionMatch match2 = re2.match(expression);
+		QRegularExpression lRegExp2("(POW\\d+.\\d+)|(POW\\d+)");
+		QRegularExpressionMatch lMatchExp2 = lRegExp2.match(lExpressionIn);
 
 
-		if (match1.hasMatch())
-		{
-			exp1 = match1.captured(0);
-			exp1.replace("POW","");
+		if(lMatchExp1.hasMatch()) {
+			lExp1 = lMatchExp1.captured(0);
+			lExp1.replace("POW","");
 		}
 
-		if (match2.hasMatch())
-		{
-			exp2 = match2.captured(0);
-			exp2.replace("POW","");
+		if(lMatchExp2.hasMatch()) {
+			lExp2 = lMatchExp2.captured(0);
+			lExp2.replace("POW","");
 		}
 
-
-		expression.replace (""+exp1+"" "POW"""+exp2+"" , "Math.pow(" ""+exp1+"" ","  ""+exp2+""  ")");
+		lExpressionIn.replace (""+lExp1+"" "POW"""+lExp2+"" , "Math.pow(" ""+lExp1+"" ","  ""+lExp2+""  ")");
 	}
 
-	expression.replace("{","(");
-	expression.replace("[","(");
-	expression.replace("]",")");
-	expression.replace ("PLUS","+");
-	expression.replace ("MINUS","-");
-	expression.replace("MULT","*");
-	expression.replace("DIV","/");
+	lExpressionIn.replace("{","(");
+	lExpressionIn.replace("[","(");
+	lExpressionIn.replace("]",")");
+	lExpressionIn.replace ("PLUS","+");
+	lExpressionIn.replace ("MINUS","-");
+	lExpressionIn.replace("MULT","*");
+	lExpressionIn.replace("DIV","/");
 
-	QScriptEngine parsexpression;
-	double result=parsexpression.evaluate(expression).toNumber();
+	QScriptEngine lScript;
+	double lResult = lScript.evaluate(lExpressionIn).toNumber();
 
-	m_textEdit->textEdit()->append(QString("> %1").arg(expression));
-	m_textEdit->textEdit()->append(QString("= %1").arg(result));
+	m_textEdit->textEdit()->append(QString("> %1").arg(lExpression));
+	m_textEdit->textEdit()->append(QString("= %1").arg(lResult));
 	m_textEdit->textEdit()->append("");
 }
 //===============================================
