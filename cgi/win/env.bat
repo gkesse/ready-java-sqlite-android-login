71932c982357ab37064472bed8ae7a4c4cc41a44
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-18 12:40:02 +0100

    Initial Commit

--- cgi/win/env.bat
+++ cgi/win/env.bat
@@ -4,8 +4,8 @@ set "PATH=C:\MinGW\bin;%PATH%"
 ::===============================================
 set "GPROJECT_ROOT=C:\Users\Admin\Downloads\Programs"
 set "GPROJECT_PATH=%GPROJECT_ROOT%\ReadyCpp"
-set "GPROJECT_SRC=%GPROJECT_PATH%\cgi\web\src"
-set "GPROJECT_WEB=%GPROJECT_PATH%\cgi\web\root"
+set "GPROJECT_SRC=%GPROJECT_PATH%\cgi\code\web\src"
+set "GPROJECT_WEB=%GPROJECT_PATH%\cgi\code\web\root"
 set "GPROJECT_BIN=%GPROJECT_WEB%\index.cgi"
 ::===============================================
 set "GPROJECT_DATA=%GPROJECT_PATH%\cgi\data"
